fa8668c6cef65bd277f753a50b4cd60823cf49ca
sagar <sagaryadav12369@gmail.com>
2025-06-11 22:07:08 +0530

    flutter ui

--- lib/view/forgot_password_screen.dart
+++ lib/view/forgot_password_screen.dart
@@ -1,13 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
 
 class ForgotPasswordScreen extends StatelessWidget {
-  const ForgotPasswordScreen({super.key});
+  ForgotPasswordScreen({super.key});
+
+  final TextEditingController _emailController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
+    final isDark = Theme.of(context).brightness == Brightness.dark;
     return Scaffold(
-      appBar: AppBar(title: const Text('Forgot Password')),
-      body: const Center(child: Text('Forgot Password Screen')),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          padding: EdgeInsets.all(24),
+          child: Column(
+            children: [
+              // back Button
+              IconButton(
+                onPressed: () => Get.back(),
+                icon: const Icon(Icons.arrow_back_ios),
+                color: isDark ? Colors.white : Colors.black,
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
