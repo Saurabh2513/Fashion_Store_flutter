ec4c5365afba7ba8a1a4c17503f6033a6e940fe4
sagar <sagaryadav12369@gmail.com>
2025-06-12 11:08:50 +0530

    flutter ui

--- lib/view/forgot_password_screen.dart
+++ lib/view/forgot_password_screen.dart
@@ -1,6 +1,6 @@
+import 'package:ecommerce_app/utils/app_textstyle.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get/get_core/src/get_main.dart';
 
 class ForgotPasswordScreen extends StatelessWidget {
   ForgotPasswordScreen({super.key});
@@ -13,14 +13,32 @@ class ForgotPasswordScreen extends StatelessWidget {
     return Scaffold(
       body: SafeArea(
         child: SingleChildScrollView(
-          padding: EdgeInsets.all(24),
+          padding: const EdgeInsets.all(16),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              // back Button
+              // back button
               IconButton(
                 onPressed: () => Get.back(),
                 icon: const Icon(Icons.arrow_back_ios),
                 color: isDark ? Colors.white : Colors.black,
+              ),
+              const SizedBox(height: 40),
+              // Welcome Text
+              Text(
+                'Reset Password',
+                style: AppTextStyle.withColor(
+                  AppTextStyle.h1,
+                  Theme.of(context).textTheme.bodyLarge!.color!,
+                ),
+              ),
+              const SizedBox(height: 8),
+              Text(
+                'enter your Email to reset the password',
+                style: AppTextStyle.withColor(
+                  AppTextStyle.bodyLarge,
+                  isDark ? Colors.grey[400]! : Colors.grey[600]!,
+                ),
               ),
             ],
           ),
