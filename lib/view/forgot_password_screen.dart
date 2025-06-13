5e23a09ec48d740843c1a168e7eb44b14c2ef236
sagar <sagaryadav12369@gmail.com>
2025-06-13 10:32:19 +0530

    flutter ui

--- lib/view/forgot_password_screen.dart
+++ lib/view/forgot_password_screen.dart
@@ -1,4 +1,5 @@
 import 'package:ecommerce_app/utils/app_textstyle.dart';
+import 'package:ecommerce_app/view/widgets/custom_textfield.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -40,9 +41,82 @@ class ForgotPasswordScreen extends StatelessWidget {
                   isDark ? Colors.grey[400]! : Colors.grey[600]!,
                 ),
               ),
+              const SizedBox(height: 40),
+              // Email TextField
+              CustomTextfield(
+                label: 'Email',
+                prefixIcon: Icons.email_outlined,
+                keyboardType: TextInputType.emailAddress,
+                controller: _emailController,
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Please enter your email';
+                  }
+                  if (!GetUtils.isEmail(value)) {
+                    return 'Please enter a valid email';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 24),
+
+              // Second Reset button
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () => _showSuccessMessage(context),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Theme.of(context).primaryColor,
+                    padding: const EdgeInsets.symmetric(vertical: 16),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(14),
+                    ),
+                  ),
+                  child: Text(
+                    "Send Reset Link",
+                    style: AppTextStyle.withColor(
+                      AppTextStyle.buttonMedium,
+                      Colors.white,
+                    ),
+                  ),
+                ),
+              ),
             ],
           ),
         ),
+      ),
+    );
+  }
+
+  // show success message
+  void _showSuccessMessage(BuildContext context) {
+    Get.dialog(
+      AlertDialog(
+        title: Text(
+          'we have sent you a reset link to your email',
+          style: AppTextStyle.buttonMedium,
+        ),
+        actions: [
+          TextButton(
+            onPressed: () => Get.back(),
+            child: Text(
+              'OK',
+              style: AppTextStyle.withColor(
+                AppTextStyle.buttonMedium,
+                Theme.of(context).primaryColor,
+              ),
+            ),
+          ),
+        ],
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(14),
+        ),
+        contentPadding: const EdgeInsets.all(16),
+        contentTextStyle: AppTextStyle.withColor(
+          AppTextStyle.buttonMedium,
+          Colors.white,
+        ),
+        elevation: 0,
       ),
     );
   }
--- lib/view/main_screen.dart
+++ lib/view/main_screen.dart
@@ -1,14 +1,17 @@
+import 'package:ecommerce_app/controllers/navigation_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
 
 class MainScreen extends StatelessWidget {
   const MainScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Main Screen"),
-      ),
+    final isDark = Theme.of(context).brightness == Brightness.dark;
+    final NavigationController navigationController = Get.put(
+      NavigationController(),
     );
+    return GetBuilder(builder: builder);
   }
 }
